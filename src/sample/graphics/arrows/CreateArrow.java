9e8b00b9a2490b19ef3947ba6151dd0caf85c5bb
Vladimir <vova435335>
2020-11-06 18:21:44 +0300

    Создание класса для отрисовки стрелок.

---
+++ src/sample/graphics/arrows/CreateArrow.java
@@ -0,0 +1,36 @@
+package sample.graphics.arrows;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class CreateArrow {
+
+    private ArrowFactory factory;
+    private Arrowhead head;
+    private ArrowLine line;
+
+    public CreateArrow(ArrowFactory factory) {
+        this.factory = factory;
+    }
+
+    private void createArrowhead() {
+        head = factory.createArrowhead();
+    }
+
+    private void paintHead(double xStart, double yStart, double xEnd, double yEnd, GraphicsContext graphicsContext) {
+        head.paint(xStart, yStart, xEnd, yEnd, graphicsContext);
+    }
+
+    private void createArrowLine() {
+        line = factory.createArrowLine();
+    }
+
+    private void paintLine(double xStart, double yStart, double xEnd, double yEnd, GraphicsContext graphicsContext) {
+        line.paint(xStart, yStart, xEnd, yEnd, graphicsContext);
+    }
+
+    public void paintArrow(double xStart, double yStart, double xEnd, double yEnd, GraphicsContext graphicsContext) {
+        paintLine(xStart, yStart, xEnd, yEnd, graphicsContext);
+        paintHead(xStart, yStart, xEnd, yEnd, graphicsContext);
+    }
+
+}
